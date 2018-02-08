277bf680788e13be3ce979d500f8885d780e04e1
nazip <nazipm@mail.ru>
2018-02-08 22:04:10 +0300

    play with webpack

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import  ReactDOM from 'react-dom';
+
+const App = (
+  <p>sssss</p>
+);
+
+ReactDOM.render(
+  App,
+  document.getElementById('root')
+);
+
+// export default App;
